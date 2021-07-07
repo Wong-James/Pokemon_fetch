2bed10de1ebff59ed9a19fada805dfbb5ed387f6
James Wong <jamesrwong310@gmail.com>
2021-07-07 12:16:27 -0700

    Super Ninja

--- Assignments/week 1/day2/ninja.js
+++ Assignments/week 1/day2/ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
-    constructor(name) {
+    constructor(name, health=100, speed=3, strength=3) {
         this.name = name;
-        this.health = 100;
-        this.speed = 3;
-        this.strength = 3;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
     }
     drinkSake() {
         this.health += 10;
@@ -13,11 +13,31 @@ class Ninja {
         return this.name
     }
     showStats() {
-        return [this.name, this.health, this.speed, this.strength];
+        return {Name: this.name, Health: this.health, Speed: this.speed, Strength: this.strength};
+    }
+
+}
+class Sensei extends Ninja {
+    constructor(name, wisdom=10){
+        super(name, 200, 10, 10);
+        this.wisdom = wisdom
+    }
+    showStats() {
+        return {Name: this.name, Health: this.health, Speed: this.speed, Strength: this.strength, Wisdom: this.wisdom};
+    }
+    speakWisdom() {
+        this.drinkSake()
+        return "What one programmer can do in one month, two programmers can do in two months."
     }
 }
 
 const ninja1 = new Ninja("Hyabusa");
 console.log(ninja1.sayName());
-console.log(ninja1.showStats());
 console.log(ninja1.drinkSake());
+console.log(ninja1.showStats());
+
+
+const superSensei = new Sensei("Master Splinter");
+console.log(superSensei.speakWisdom());
+console.log(superSensei.showStats());
+
